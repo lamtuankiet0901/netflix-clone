647acc467cebb21e0a091b0414e566bf25bb1b71
Lam Tuan Kiet <kietlam0901@gmail.com>
2021-07-04 15:47:15 +0700

    Create Navbar, Featured

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import Featured from "../../components/featured/Featured"
+import Navbar from "../../components/navbar/Navbar"
+import "./home.scss"
+
+export const Home = () => {
+    return (
+        <div className="home">
+            <Navbar />
+            <Featured type={"movie"}/>
+        </div>
+    )
+}
