dc59176bf9c8ac6d1dc6fd1e7ae933fbeea9404f
Lam Tuan Kiet <kietlam0901@gmail.com>
2021-07-06 15:55:23 +0700

    Create Home, Featured

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import "./app.css"
+import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Home />
+      </div>
+    </>
   );
 }
 
---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+    {
+      name: 'Jan',
+      "Active User": 4000,
+    },
+    {
+      name: 'Feb',
+      "Active User": 3000,
+    },
+    {
+      name: 'Mar',
+      "Active User": 2000,
+    },
+    {
+      name: 'Apr',
+      "Active User": 2780,
+    },
+    {
+      name: 'May',
+      "Active User": 1890,
+    },
+    {
+      name: 'Jun',
+      "Active User": 2390,
+    },
+    {
+      name: 'July',
+      "Active User": 3490,
+    },
+    {
+      name: 'Aug',
+      "Active User": 4100,
+    },
+    {
+      name: 'Sep',
+      "Active User": 2980,
+    },
+    {
+      name: 'Oct',
+      "Active User": 3600,
+    },
+    {
+      name: 'Nov',
+      "Active User": 4100,
+    },
+    {
+      name: 'Dec',
+      "Active User": 3900,
+    },
+  ];
