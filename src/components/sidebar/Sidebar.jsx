08dbbd64faeb4d1e3b6488a4bcf2b97d48f02e1c
Lam Tuan Kiet <kietlam0901@gmail.com>
2021-07-07 16:49:54 +0700

    Upload final

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -10,6 +10,7 @@ import { LineStyle,
     ChatBubbleOutline,
     WorkOutline,
     Report, } from "@material-ui/icons"
+import { Link } from "react-router-dom"
 import "./sidebar.css"
 
 export default function Sidebar() {
@@ -19,10 +20,12 @@ export default function Sidebar() {
                 <div className="sidebarMenu">
                     <h3 className="sidebarTitle">Dashboard</h3>
                     <ul className="sidebarList">
-                        <li className="sidebarListItem active">
-                            <LineStyle className="sidebarIcon" />
-                            Home
-                        </li>
+                        <Link to="/" className="link">
+                            <li className="sidebarListItem active">
+                                <LineStyle className="sidebarIcon" />
+                                Home
+                            </li>
+                        </Link>
                         <li className="sidebarListItem">
                             <Timeline className="sidebarIcon" />
                             Analytisc
@@ -36,14 +39,18 @@ export default function Sidebar() {
                 <div className="sidebarMenu">
                     <h3 className="sidebarTitle">Quick Menu</h3>
                     <ul className="sidebarList">
-                        <li className="sidebarListItem active">
-                            <PermIdentity  className="sidebarIcon" />
-                            Users
-                        </li>
-                        <li className="sidebarListItem">
-                            <Storefront  className="sidebarIcon" />
-                            Products
-                        </li>
+                        <Link to="/users" className="link" >
+                            <li className="sidebarListItem active">
+                                <PermIdentity  className="sidebarIcon" />
+                                Users
+                            </li>
+                        </Link>
+                        <Link to="/products" className="link" >
+                            <li className="sidebarListItem">
+                                <Storefront  className="sidebarIcon" />
+                                Products
+                            </li>
+                        </Link>
                         <li className="sidebarListItem">
                             <AttachMoney  className="sidebarIcon" />
                             Transactions
